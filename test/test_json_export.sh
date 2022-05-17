a80d209a0adfa19039f0b423da7f0304690c88e2
chenlianghong <clh021@gmail.com>
2022-05-17 17:16:18 +0800

    update test_json_export.sh

--- test/test_json_export.sh
+++ test/test_json_export.sh
@@ -14,8 +14,15 @@ fi
 cd "$BUILD_DIR"
 cmake ..
 make
-sudo make install
+cp *.so ../lib/
+cp *.so ../share/
+#sudo make install
+#sudo rm -rf /usr/local/share/hardinfo
+#sudo rm -rf /usr/local/lib/hardinfo
+#sudo rm -rf /usr/local/bin/hardinfo
+#sudo rm -f /usr/local/share/man/man1/hardinfo.1
+#sudo rm -f /usr/local/share/applications/hardinfo.desktop
+
+./hardinfo -r -m devices.so -a -f json > "$ProjectPath/build/test.output.json"
 
 cd "$OldPath"
-
-hardinfo -r -m devices.so -a -f json > "$ProjectPath/build/test.output.json"
